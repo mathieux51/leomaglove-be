785d97cde708f19abff65ca683d2a54ef1eedfb6
Mathieu <mathieux51@gmail.com>
2019-02-26 07:58:13 +0100

    Deploy to gh-pages

---
+++ out/_next/static/webpack/static/development/pages/_app.js.f946ebb56dc5fbe8c7fc.hot-update.js
@@ -0,0 +1,80 @@
+webpackHotUpdate("static/development/pages/_app.js",{
+
+/***/ "./components/Header.js":
+/*!******************************!*\
+  !*** ./components/Header.js ***!
+  \******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Header; });
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_classCallCheck__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/classCallCheck */ "./node_modules/@babel/runtime-corejs2/helpers/esm/classCallCheck.js");
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_createClass__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/createClass */ "./node_modules/@babel/runtime-corejs2/helpers/esm/createClass.js");
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_possibleConstructorReturn__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/possibleConstructorReturn */ "./node_modules/@babel/runtime-corejs2/helpers/esm/possibleConstructorReturn.js");
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_getPrototypeOf__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/getPrototypeOf */ "./node_modules/@babel/runtime-corejs2/helpers/esm/getPrototypeOf.js");
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_inherits__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/inherits */ "./node_modules/@babel/runtime-corejs2/helpers/esm/inherits.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_5__);
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! styled-components */ "./node_modules/styled-components/dist/styled-components.browser.esm.js");
+
+
+
+
+
+var _jsxFileName = "/Users/mathieu/Projects/production/leomaglove-be/components/Header.js";
+
+
+var Container = styled_components__WEBPACK_IMPORTED_MODULE_6__["default"].div.attrs({
+  className: "flex jc-c ai-c"
+}).withConfig({
+  displayName: "Header__Container",
+  componentId: "sc-1l4pn8m-0"
+})(["height:100px;border:1px solid black;"]);
+var H1 = styled_components__WEBPACK_IMPORTED_MODULE_6__["default"].h1.attrs({
+  className: ""
+}).withConfig({
+  displayName: "Header__H1",
+  componentId: "sc-1l4pn8m-1"
+})(["margin:0;"]);
+
+var Header =
+/*#__PURE__*/
+function (_React$PureComponent) {
+  Object(_babel_runtime_corejs2_helpers_esm_inherits__WEBPACK_IMPORTED_MODULE_4__["default"])(Header, _React$PureComponent);
+
+  function Header() {
+    Object(_babel_runtime_corejs2_helpers_esm_classCallCheck__WEBPACK_IMPORTED_MODULE_0__["default"])(this, Header);
+
+    return Object(_babel_runtime_corejs2_helpers_esm_possibleConstructorReturn__WEBPACK_IMPORTED_MODULE_2__["default"])(this, Object(_babel_runtime_corejs2_helpers_esm_getPrototypeOf__WEBPACK_IMPORTED_MODULE_3__["default"])(Header).apply(this, arguments));
+  }
+
+  Object(_babel_runtime_corejs2_helpers_esm_createClass__WEBPACK_IMPORTED_MODULE_1__["default"])(Header, [{
+    key: "render",
+    value: function render() {
+      return react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement(Container, {
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 14
+        },
+        __self: this
+      }, react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement(H1, {
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 15
+        },
+        __self: this
+      }, "L\xE9o \u2764\uFE0F Magali"));
+    }
+  }]);
+
+  return Header;
+}(react__WEBPACK_IMPORTED_MODULE_5___default.a.PureComponent);
+
+
+
+/***/ })
+
+})
+//# sourceMappingURL=_app.js.f946ebb56dc5fbe8c7fc.hot-update.js.map
---
+++ out/_next/static/webpack/static/development/pages/index.js.204e7335e3b973194e9e.hot-update.js
@@ -0,0 +1,122 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./components/Body.js":
+/*!****************************!*\
+  !*** ./components/Body.js ***!
+  \****************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! styled-components */ "./node_modules/styled-components/dist/styled-components.browser.esm.js");
+var _jsxFileName = "/Users/mathieu/Projects/production/leomaglove-be/components/Body.js";
+
+
+var Main = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].main.attrs({
+  className: "flex fxd-c"
+}).withConfig({
+  displayName: "Body__Main",
+  componentId: "sc-1ccgya6-0"
+})(["min-height:calc(100vh - 100px);"]);
+var VideoContainer = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].div.attrs({
+  className: "flex-1 jc-c ai-c"
+}).withConfig({
+  displayName: "Body__VideoContainer",
+  componentId: "sc-1ccgya6-1"
+})(["margin:20.9% 0;"]);
+var MoreInfoContainer = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].div.attrs({
+  className: "flex fxd-c ai-c jc-sb"
+}).withConfig({
+  displayName: "Body__MoreInfoContainer",
+  componentId: "sc-1ccgya6-2"
+})(["padding:0 5vw 20px;background:hotpink;"]);
+var TextInfo = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].span.attrs({
+  className: ""
+}).withConfig({
+  displayName: "Body__TextInfo",
+  componentId: "sc-1ccgya6-3"
+})(["margin:10px 10px 20px;"]);
+var ButtonContainer = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].div.attrs({
+  className: "flex jc-sb w100"
+}).withConfig({
+  displayName: "Body__ButtonContainer",
+  componentId: "sc-1ccgya6-4"
+})([""]);
+var Button = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].button.attrs({
+  className: ""
+}).withConfig({
+  displayName: "Body__Button",
+  componentId: "sc-1ccgya6-5"
+})(["border:1px solid black;padding:20px;"]);
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(Main, {
+    id: "main",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 30
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(VideoContainer, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 31
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 32
+    },
+    __self: this
+  }, "Vid\xE9o en boucle")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(MoreInfoContainer, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 34
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(TextInfo, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 35
+    },
+    __self: this
+  }, "Plus d'infos"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(ButtonContainer, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 36
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(Button, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 37
+    },
+    __self: this
+  }, "Les invit\xE9s en ", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("strong", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 38
+    },
+    __self: this
+  }, " Belgique")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(Button, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 40
+    },
+    __self: this
+  }, "Les invit\xE9s en ", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("strong", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 41
+    },
+    __self: this
+  }, " France")))));
+});
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.204e7335e3b973194e9e.hot-update.js.map
