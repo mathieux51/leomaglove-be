2f208ba2aded560c30d48b87e8f5cec698f189c1
Mathieu <mathieux51@gmail.com>
2019-03-17 10:09:14 +0100

    Add hotjar

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import React from "react"
+import { hotjar } from "react-hotjar"
 import App, { Container } from "next/app"
 import Layout from "components/Layout"
 
 export default class extends App {
+  componentDidMount() {
+    if (process.env.NODE_ENV !== "development") {
+      hotjar.initialize(process.env.HOT_JAR_SITE_ID)
+    }
+  }
   render() {
     const { Component, pageProps } = this.props
     return (
