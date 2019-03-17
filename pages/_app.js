0178ef9ddaa40e4d89b5f97c6432c9951149ec16
Mathieu <mathieux51@gmail.com>
2019-03-17 10:23:23 +0100

    Add GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import React from "react"
 import { hotjar } from "react-hotjar"
+import ReactGA from "react-ga"
 import App, { Container } from "next/app"
 import Layout from "components/Layout"
 
 export default class extends App {
   componentDidMount() {
     if (process.env.NODE_ENV !== "development") {
-      hotjar.initialize(process.env.HOT_JAR_SITE_ID)
+      hotjar.initialize(process.env.HOT_JAR_SITE_ID, "v1")
+      ReactGA.initialize(process.env.GA_TRACKING_ID)
+      ReactGA.pageview(window.location.pathname + window.location.search)
     }
   }
   render() {
