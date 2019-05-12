e904ed4003f9c5b382ead387cab2b1f9f30fa645
Mathieu <mathieux51@gmail.com>
2019-05-12 10:10:22 +0200

    Remove logs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,13 +6,10 @@ import Layout from "../components/Layout"
 import { InformationProvider } from "../context/InformationContext"
 
 const isProd = process.env.NODE_ENV !== "development"
-console.info("process.env", JSON.stringify(process.env, null, 2))
 
 export default class extends App {
   componentDidMount() {
     if (isProd) {
-      console.info("process.env.HOT_JAR_SITE_ID", process.env.HOT_JAR_SITE_ID)
-      console.info("process.env.GA_TRACKING_ID", process.env.GA_TRACKING_ID)
       hotjar.initialize(process.env.HOT_JAR_SITE_ID || "", "v1")
       ReactGA.initialize(process.env.GA_TRACKING_ID || "")
       ReactGA.pageview(window.location.pathname + window.location.search)
