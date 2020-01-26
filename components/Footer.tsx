1acbd765adc9b36ad9553160e657eddb3768cefc
Mathieu <mathieux51@gmail.com>
2020-01-26 15:36:38 +0100

    Fix React imports

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import React from "react"
 import styled from "styled-components"
 
 const StyledFooter = styled.footer.attrs({ className: "flex jc-c ai-c" })`
--- components/Information.tsx
+++ components/Information.tsx
@@ -1,4 +1,4 @@
-// import * as React from "react"
+// import React from "react"
 // import styled from "styled-components"
 // import posed, { PoseGroup } from "react-pose"
 // import MapIFrame from "./MapIFrame"
