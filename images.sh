c11e8386828d5227af0a7b049a70f7d65cf656cd
Mathieu <mathieux51@gmail.com>
2020-02-01 15:31:57 +0100

    Add Elvis pictures

--- images.sh
+++ images.sh
@@ -6,7 +6,7 @@
 #  rename "s/_//g" *.jpg
 
 node scripts/images.js docs/PHOTOFLASH\ \(by\ everybody\) everybody
-node scripts/images.js docs/PHOTOS\ CADEAUX\ \(by\ Emilie\ et\ Kaspar\) emilie-and-kaspar
+node scripts/images.js docs/PHOTOS\ CADEAUX\ \(by\ Emilie,\ Elvis\ et\ Kaspar\) emilie-elvis-kaspar
 node scripts/images.js docs/PHOTOS\ OFFICIELLES\ by\ Amélie\ Landry/3-LE\ SUD\ la\ totale france
 node scripts/images.js docs/PHOTOS\ OFFICIELLES\ by\ Amélie\ Landry/2-BELGIQUE\ la\ totale belgium
 node scripts/images.js docs/PHOTOS\ OFFICIELLES\ by\ Amélie\ Landry/1-La\ crème\ de\ la\ crème official
